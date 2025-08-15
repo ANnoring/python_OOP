3b0ac736a33239b2d57ca0e9d19d393114fa0128
ANnoring <zerofrist10@gmail.com>
2025-08-15 10:56:56 +0700

    Create AccessModifier.py

---
+++ AccessModifier.py
@@ -0,0 +1,29 @@
+# AccessModifier
+# คือระดับในการเข้าถึง class attribute method และอื่นๆ ในภาษาเชิงวัตถุ
+# โดยมีประโยชน์อย่างมากในเรื่องของการกำหนดระดับการเข้าถึง
+# สิทธิในการเข้าใช้งาน การซ้อนข้อมูล และอื่นๆ
+
+# Public คือการประกาศระดับการเข้าถึงที่เข้มงวดน้อยทีสุด หรือกล่าวได้ว่าใครๆ ก็สามารถเข้าถึงการใช้งานได้
+
+# Protected(_) เป็นการประกาศระดับการเข้าถึงเฉพาะคลาสขอตัวเองและคลาสลูกที่สืบทอดคุณสมบัติ
+
+# Prive(_) เป็นการประกาศระดับการเข้าถึงที่เข้มงวดที่สุด กล่าวคือ จะมีแต่คลาสของ
+# ตัวมันเองเท่านั้นที่มสิทธิ์ใช้งานได้
+
+class Employee:
+    def __init__(self, name, salary, department):
+        # public attribute
+        self.name = name
+        self.salary = salary
+        self.department = department
+
+    # public method
+    def showData(self):
+        print('ชื่อพนักงาน = {}'.format(self.name))
+        print('เงินเดือน  = {}'.format(self.salary))
+        print('แผนก  = {}'.format(self.department))
+
+
+emp1 = Employee('วิบูลย์', 50000, 'ธุระกิจ')
+emp1.salary = 51000
+emp1.showData()
